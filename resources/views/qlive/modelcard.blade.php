d1268039545f0fd01af76c439f685f5dda14608b
李杰儒 <lee0936996319b@gmail.com>
2019-11-11 10:28:20 +0800

    q-live modelcard

--- resources/views/qlive/modelcard.blade.php
+++ resources/views/qlive/modelcard.blade.php
@@ -6,56 +6,61 @@
     <div class="modelcardPage page＿padding_top pageContainer">
         <div class="container">
             <h2 class="subtitle u-text-center bounceInUp">Qlive雜誌_合作夥伴</h2>
-             
+
             <div class="row ">
 
                 <div class="col-sm-4  wow fadeInLeftBig u-mb-24" data-wow-delay="0.8s">
-                    <div class="modelcard_imge">
-                        <div class="imgbox u-BorderRradius-8">
-                            <div class="imgbox__inner imgbox__inner-3-4 ">
-                                <div class="image " style="background-image: url(images/model/p4.jpg);"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <img class="img" src="images/model/p4.jpg" alt="">
+{{--                    <div class="modelcard_imge">--}}
+{{--                        <div class="imgbox u-BorderRradius-8">--}}
+{{--                            <div class="imgbox__inner imgbox__inner-3-4 ">--}}
+{{--                                <div class="image " style="background-image: url(images/model/p4.jpg);"></div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
                 <div class="col-sm-4  wow zoomIn  u-mb-24" data-wow-delay="0.8s">
-                    <div class="modelcard_imge">
-                        <div class="imgbox u-BorderRradius-8">
-                            <div class="imgbox__inner imgbox__inner-3-4 ">
-                                <div class="image " style="background-image: url(images/model/70559172_923376558018850_5245098032780804096_o.jpg);"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <img class="img" src="images/model/70559172_923376558018850_5245098032780804096_o.jpg" alt="">
+{{--                    <div class="modelcard_imge">--}}
+{{--                        <div class="imgbox u-BorderRradius-8">--}}
+{{--                            <div class="imgbox__inner imgbox__inner-3-4 ">--}}
+{{--                                <div class="image " style="background-image: url(images/model/70559172_923376558018850_5245098032780804096_o.jpg);"></div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
                 <div class="col-sm-4  wow fadeInRightBig u-mb-24" data-wow-delay="0.8s">
-                    <div class="modelcard_imge">
-                        <div class="imgbox u-BorderRradius-8">
-                            <div class="imgbox__inner imgbox__inner-3-4 ">
-                                <div class="image " style="background-image: url(images/model/p3.jpg);"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <img class="img" src="images/model/p3.jpg" alt="">
+{{--                    <div class="modelcard_imge">--}}
+{{--                        <div class="imgbox u-BorderRradius-8">--}}
+{{--                            <div class="imgbox__inner imgbox__inner-3-4 ">--}}
+{{--                                <div class="image " style="background-image: url(images/model/p3.jpg);"></div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
 
             </div>
             <div class="row">
                 <div class="col-sm-6  wow fadeInUpBig u-mb-24" data-wow-delay="0.8s">
-                    <div class="modelcard_imge">
-                        <div class="imgbox u-BorderRradius-8">
-                            <div class="imgbox__inner imgbox__inner-4-3 ">
-                                <div class="image " style="background-image: url(images/model/p2.jpg);"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <img class="img" src="images/model/p2.jpg" alt="">
+{{--                    <div class="modelcard_imge">--}}
+{{--                        <div class="imgbox u-BorderRradius-8">--}}
+{{--                            <div class="imgbox__inner imgbox__inner-4-3 ">--}}
+{{--                                <div class="image " style="background-image: url(images/model/p2.jpg);"></div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
                 <div class="col-sm-6  wow fadeInUpBig u-mb-24" data-wow-delay="0.8s">
-                    <div class="modelcard_imge">
-                        <div class="imgbox u-BorderRradius-8">
-                            <div class="imgbox__inner imgbox__inner-4-3 ">
-                                <div class="image " style="background-image: url(images/model/p1.jpg);"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <img class="img" src="images/model/p1.jpg" alt="">
+{{--                    <div class="modelcard_imge">--}}
+{{--                        <div class="imgbox u-BorderRradius-8">--}}
+{{--                            <div class="imgbox__inner imgbox__inner-4-3 ">--}}
+{{--                                <div class="image " style="background-image: url(images/model/p1.jpg);"></div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
 
             </div>
@@ -68,7 +73,7 @@
                     <li class=" wow bounceIn" data-wow-delay="1.5s">鞋號</li>
                     <li class=" wow bounceIn" data-wow-delay="1.8s">FB追蹤人數</li>
                     <li class=" wow bounceIn" data-wow-delay="2.1s">IG追蹤人數</li>
-                    
+
                 </ul>
             </div>
 
